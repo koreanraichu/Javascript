2224f57ddcfe02409003dad37b31237f1e3ea212
koreanraichu <blackholekun@gmail.com>
2024-01-16 23:42:04 +0900

    코드 보완사항 있습니다
    1. 현재 날짜보다 미래 날짜를 입력할 때에 대한 처리(alert)
    2. CSS 조장(마진 패딩 이런거)
    3. 현재 날짜 기재
    4. 입력 날짜 최댓값 최솟값 지정(1900-01-01~2900-12-31)

--- howoldareyou/script.js
+++ howoldareyou/script.js
@@ -2,6 +2,7 @@ const birthday = document.querySelector('input[type="date"]');
 const calButton = document.querySelector('button');
 const yourAge = document.createElement('p');
 const yourAge_div = document.querySelector('.yourage');
+const now = document.querySelector('.nowdate');
 
 // 현재 날짜
 const today = new Date;
@@ -20,9 +21,16 @@ calButton.addEventListener('click',function(event){
         yourAge.innerText = `만 ${todayYear - new Date(birthday.value).getFullYear()}세입니다. `;
         yourAge_div.appendChild(yourAge);
     }
+    else if (todayYear - new Date(birthday.value).getFullYear() -1 < 0) { //생년월일로 미래의 날짜를 입력한다면?
+        alert('미래 날짜로 만 나이를 계산할 수 없습니다!') //응 계산 못해요 
+
+    }
     else { //아녀?
         //응 멀었어
         yourAge.innerText = `만 ${todayYear - new Date(birthday.value).getFullYear() - 1}세입니다. `;
         yourAge_div.appendChild(yourAge);
     }
 })
+
+now.innerText = `오늘은 ${todayYear}년 ${todayMonth}월 ${todayDate}일입니다. `;
+//오늘 날짜 표시란 
