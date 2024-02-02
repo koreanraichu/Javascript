22ac9ea7f04df214af47187876eb24b69532fe50
Koreanraichu <blackholekun@gmail.com>
2024-02-03 02:31:07 +0900

    Random ID Generator
    1. 일단 '돌아는' 갑니다. '돌아는' 가요.
    2. 체크박스를 선택하면 옵션에 따라 글자를 만드는데... 이게 코드를 보시면 아시겠지만 value가 HTML에 있거든요... 이거 자바스크립트로 빼버리고 싶은데 방법을 모르겠습니다. checkbox value에는 숫자 영소 영대 이런 식으로 주고 자바스크립트에서 숫자가 선택됐으면 0~9, 영소는 a~z, 영대는 A~Z 이런 식으로 주고 싶었습니다. if문 썼는데 안되더군요.
    3. Reset버튼 누르면 페이지가 새로고침 됩니다. div에 p태그 만들어서 붙이는 구조라 그게 최선이었음둥...

---
+++ random ID generator/script.js
@@ -0,0 +1,39 @@
+const button = document.querySelectorAll('button');
+const idLengthval = document.querySelector('.idlength');
+const idCount = document.querySelector('.idhowmany');
+const idValarea = document.querySelector(".ID");
+const wordOption = document.querySelectorAll(".listopt");
+
+function makelist() {
+    let word_list = "";
+    wordOption.forEach((item) => {
+        if (item.checked) {
+            word_list += item.value
+        }
+    })
+    return word_list
+}
+
+function makeID() {
+    let idLength = idLengthval.value;
+    let idText = "";
+    let word_list = makelist();
+    let idValue = document.createElement("p");
+    //자바스크립트는 아스키코드 못다루나? 
+    for (let i = 0;i < idLength;i++) {
+        idText += word_list.charAt(Math.floor(Math.random() * word_list.length));
+        idValue.innerText = idText;
+        idValarea.appendChild(idValue);
+    };
+}
+
+button[0].addEventListener('click',(e) => {
+    let idCountvalue = idCount.value;
+    for (let i = 0;i < idCountvalue;i++){
+        makeID();
+    };
+});
+
+button[1].addEventListener('click',(e)=>{
+    location.reload(true);
+});
