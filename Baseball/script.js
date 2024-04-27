f46f1bc4fb4080b32346ec35db1e31ed91bcb54d
Koreanraichu <blackholekun@gmail.com>
2024-04-28 02:40:48 +0900

    숫자 야구
    자바스크립트로도 만들 수 있다니...
    1. 아웃/정답 글자색을 다르게 해봤습니다.
    2. 중복된 숫자를 입력하거나(예: 111) 세자리가 아니면 알림창이 뜹니다.
    3. 코드펜에도 올렸습니다.
    
    https://codepen.io/koreanraichu/pen/VwNNZGW

---
+++ Baseball/script.js
@@ -0,0 +1,66 @@
+const ballInput = document.querySelector('.ballinput');
+const ballButton = document.querySelector('.ballbutton');
+const ballAnswer = document.querySelector('.answer');
+// 입력창이랑 버튼은 바뀌는거 없으니깐 const로 가져왔습니다. 
+
+let round = 0;
+let strike = 0;
+let ball = 0;
+// 몇트째인가, 스트라이크, 볼
+
+let numberArray = ['1','2','3','4','5','6','7','8','9'];
+let ballQuestion = [];
+for (let i = 0;i < 3;i++) {
+    let ballPick = [];
+    ballPick = numberArray.splice(Math.floor(Math.random() * (numberArray.length - i)),1)[0];
+    ballQuestion.push(ballPick);
+}
+// 배열 만들기
+
+ballButton.addEventListener('click',()=>{
+    if (ballInput.value.length != 3) {
+        strike = 0; //스트라이크
+        ball = 0; //볼
+        alert('세 자리 숫자를 입력해주세요! ');
+        ballInput.value = '';
+    }
+    else if (new Set(ballInput.value).size < 3) {
+        strike = 0; //스트라이크
+        ball = 0; //볼
+        alert('숫자는 중복되지 않는 세 자리 숫자입니다. ');
+        ballInput.value = '';
+    }
+    else {
+        strike = 0; //스트라이크
+        ball = 0; //볼
+        round ++;
+        for (let i = 0;i < 3;i++) {
+            if (ballQuestion[i] == ballInput.value[i]) {
+                strike ++;
+            }
+            else if (ballInput.value.indexOf([i]) != -1) {
+                ball ++;
+            }
+        };
+        if (ball == 0 && strike == 0) {
+            let ballText = document.createElement('p');
+            ballText.innerText = `Round ${round}: ${ballInput.value}, Out! 😥`;
+            ballText.classList.add('nohit');
+            ballAnswer.appendChild(ballText);
+        }
+        else if (strike == 3) {
+            let ballText = document.createElement('p');
+            ballText.innerText = `Round ${round}: ${ballInput.value}, ${strike}S ${ball}B ⚾`;
+            ballText.classList.add('homerun');
+            ballAnswer.appendChild(ballText);
+        }
+        else {
+            let ballText = document.createElement('p');
+            ballText.innerText = `Round ${round}: ${ballInput.value}, ${strike}S ${ball}B`;
+            ballAnswer.appendChild(ballText);
+        }
+        ballInput.value = '';
+    }
+});
+//아 정말 힘들다... 
+
