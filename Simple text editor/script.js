776fb529049a7578e070d534e1c06f6ab784c143
Koreanraichu <blackholekun@gmail.com>
2025-08-14 21:20:09 +0900

    약간의 수정사항
    1. 컬러 테이블 변경
    2. 저장 아이콘 변경(다운로드하는거라서 다운로드 아이콘으로 변경했습니다)
    3. 클립보드 아이콘 추가(누르면 텍스트에리어 속 내용물이 북사됩니다)
    4. 기타 소소한 수정들

--- Simple text editor/script.js
+++ Simple text editor/script.js
@@ -1,7 +1,9 @@
 const textTitle = document.querySelector('#title');
 const textArea = document.querySelector('#textarea');
 const textSave = document.querySelector('#save');
+const textCopy = document.querySelector('#clipbrd');
 const count = document.querySelector('.count');
+const Copied = document.querySelector('.copied');
 
 textSave.addEventListener('click',()=>{
     const blob = new Blob([textArea.value], {type:'text/plain'});
@@ -12,10 +14,10 @@ textSave.addEventListener('click',()=>{
     link.style.display = 'none';
     link.href = url;
     if (textTitle.value.trim().length == 0) {
-        link.download = 'Untitled';
+        link.download = 'Untitled.txt';
     }
     else {
-        link.download = textTitle.value;
+        link.download = `${textTitle.value}.txt`;
     }
 
     document.body.appendChild(link);
@@ -31,3 +33,11 @@ textSave.addEventListener('click',()=>{
 textArea.addEventListener('keyup',()=>{
     count.innerText = textArea.value.length;
 });
+
+textCopy.addEventListener('click',()=>{
+    window.navigator.clipboard.writeText(textArea.value);
+    Copied.style.top = "1%";
+    setTimeout(() => {
+        Copied.style.top = "-10%";
+    }, 500)
+})
