5b2d919f4f67e2246a3815a5cd6abf3bf1eaef57
Koreanraichu <blackholekun@gmail.com>
2025-08-18 23:41:29 +0900

    에디터 기능 추가
    Select태그에 있는 글꼴들을 누르면 작성란 글꼴이 해당 글꼴로 변경됩니다.
    
    https://koreanraichu.tistory.com/718 <<자세한 것은 여기를 참고해주세요.

--- Simple text editor/script.js
+++ Simple text editor/script.js
@@ -4,6 +4,7 @@ const textSave = document.querySelector('#save');
 const textCopy = document.querySelector('#clipbrd');
 const count = document.querySelector('.count');
 const Copied = document.querySelector('.copied');
+const fontSelection = document.querySelector('#fonts');
 
 textSave.addEventListener('click',()=>{
     const blob = new Blob([textArea.value], {type:'text/plain'});
@@ -41,3 +42,43 @@ textCopy.addEventListener('click',()=>{
         Copied.style.top = "-10%";
     }, 500)
 })
+
+fontSelection.addEventListener('click',()=>{
+    if (fontSelection.selectedIndex == 0) {
+        fontSelection.style.fontFamily = 'Ownglyph_corncorn-Rg';
+        textTitle.style.fontFamily = 'Ownglyph_corncorn-Rg';
+        textArea.style.fontFamily = 'Ownglyph_corncorn-Rg';
+    } else if (fontSelection.selectedIndex == 1) {
+        fontSelection.style.fontFamily = 'CHOGOONCHICKENSCRATCHV7';
+        textTitle.style.fontFamily = 'CHOGOONCHICKENSCRATCHV7';
+        textArea.style.fontFamily = 'CHOGOONCHICKENSCRATCHV7';
+    } else if (fontSelection.selectedIndex == 2) {
+        fontSelection.style.fontFamily = 'nanumgothic';
+        textTitle.style.fontFamily = 'nanumgothic';
+        textArea.style.fontFamily = 'nanumgothic';
+    } else if (fontSelection.selectedIndex == 3) {
+        fontSelection.style.fontFamily = 'Pretendard-Regular';
+        textTitle.style.fontFamily = 'Pretendard-Regular';
+        textArea.style.fontFamily = 'Pretendard-Regular';
+    } else if (fontSelection.selectedIndex == 4) {
+        fontSelection.style.fontFamily = 'RIDIBatang';
+        textTitle.style.fontFamily = 'RIDIBatang';
+        textArea.style.fontFamily = 'RIDIBatang';
+    } else if (fontSelection.selectedIndex == 5) {
+        fontSelection.style.fontFamily = 'DOSIyagiMedium';
+        textTitle.style.fontFamily = 'DOSIyagiMedium';
+        textArea.style.fontFamily = 'DOSIyagiMedium';
+    } else if (fontSelection.selectedIndex == 6) {
+        fontSelection.style.fontFamily = 'MabinogiClassicR';
+        textTitle.style.fontFamily = 'MabinogiClassicR';
+        textArea.style.fontFamily = 'MabinogiClassicR';
+    } else if (fontSelection.selectedIndex == 7) {
+        fontSelection.style.fontFamily = 'MaplestoryOTFBold';
+        textTitle.style.fontFamily = 'MaplestoryOTFBold';
+        textArea.style.fontFamily = 'MaplestoryOTFBold';
+    } else if (fontSelection.selectedIndex == 8) {
+        fontSelection.style.fontFamily = 'CookieRun-Regular';
+        textTitle.style.fontFamily = 'CookieRun-Regular';
+        textArea.style.fontFamily = 'CookieRun-Regular';
+    }
+});
